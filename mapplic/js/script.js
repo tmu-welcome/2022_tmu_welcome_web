a80835612ee9ef0abf50ec111a19edfa5044c8c7
Sizumi <souta120789@gmail.com>
2022-03-29 16:03:40 +0900

    map fix

--- mapplic/js/script.js
+++ mapplic/js/script.js
@@ -1,19 +1,103 @@
-/* Landing page scripts */
-$(document).ready(function() {
-	$('.usage').click(function(e) {
+$(document).ready(function() { 
+	$(function(){
+		var $isScrolling = 0 ;
+		var $timeoutId ;
+
+		$(document).on( "scroll", function () {
+			$isScrolling = 1 ;
+
+		// スクロールを停止して200ms後に終了とする
+		clearTimeout( $timeoutId );
+
+		$timeoutId = setTimeout( function () {
+			$isScrolling = 0 ;
+		}, 200 );
+		});
+	});
+
+	var clickEventType = (( window.ontouchstart!==null ) ? 'click':'touchend');
+	var css = '.mapplic-filtered svg [id^=landmark] > * {opacity: 1 !important; }';
+	var map_f = $('#mapplic_f').mapplic({
+		source: 'map_f.json',
+		customcss: css,
+		sidebar: false,
+		sidebartoggle: true,
+		alphabetic: true,
+		height: 'auto',
+		developer: false,
+		searchdescription: true,
+		searcheverywhere: true,
+		animations: true,
+		minimap: true,
+		marker: 'hidden',
+		fillcolor: false,
+		fullscreen: false,
+		thumbholder: true,
+		maxscale: 3
+	});
+
+	var self_f = map_f.data('mapplic');
+
+	$(document).on(clickEventType, '#infome-building1f', function (e) {
 		e.preventDefault();
-		$('.editor-window').slideToggle(200);
+		self_f.switchLevel('floor-1-1-first');
+	});
+	
+	$(document).on(clickEventType, '#infome-building7f', function (e) {
+		e.preventDefault();
+		self_f.switchLevel('floor-7-1-first');
 	});
 
-	$(document).on('mousemove', '.mapplic-layer', function(e) {
-		var map = $('.mapplic-map'),
-			x = (e.pageX - map.offset().left) / map.width(),
-			y = (e.pageY - map.offset().top) / map.height();
-		$('.mapplic-coordinates-x').text(parseFloat(x).toFixed(4));
-		$('.mapplic-coordinates-y').text(parseFloat(y).toFixed(4));
+	$(document).on(clickEventType, '#infome-hallf', function (e) {
+		e.preventDefault();
+		self_f.switchLevel('hall');
 	});
 
-	$('.editor-window .window-mockup').click(function() {
-		$('.editor-window').slideUp(200);
+	$(document).on(clickEventType, '#hall-building1', function (e) {
+		e.preventDefault();
+		self_f.switchLevel('floor-1-1-first');
+	});
+
+
+	var map_s = $('#mapplic_s').mapplic({
+		source: 'map_s.json',
+		customcss: css,
+		sidebar: false,
+		sidebartoggle: true,
+		alphabetic: true,
+		height: 'auto',
+		developer: true,
+		searchdescription: true,
+		searcheverywhere: true,
+		animations: true,
+		minimap: true,
+		marker: 'hidden',
+		fillcolor: false,
+		fullscreen: false,
+		thumbholder: true,
+		maxscale: 3
+	});
+
+	var self_s = map_s.data('mapplic');
+
+	$(document).on(clickEventType, '#infome-building1s', function (e) {
+		e.preventDefault();
+		self_s.switchLevel('floor-1-1-second');
+	});
+
+	$(document).on(clickEventType, '#infome-building7s', function (e) {
+		e.preventDefault();
+		self_s.switchLevel('floor-7-1-second');
+	});
+
+
+	$(document).on(clickEventType, '#infome-halls', function (e) {
+		e.preventDefault();
+		self_s.switchLevel('hall');
+	});
+
+	$(document).on(clickEventType, '#hall-building1', function (e) {
+		e.preventDefault();
+		self_s.switchLevel('floor-1-1-first');
 	});
 });
