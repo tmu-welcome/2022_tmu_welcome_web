332ad8cb195fe713df206b65ff516951934de384
tmu-welcome <tyuushin@gmail.com>
2022-04-06 14:33:56 +0900

    fix map

--- mapplic/js/script.js
+++ mapplic/js/script.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 		sidebartoggle: true,
 		alphabetic: true,
 		height: 'auto',
-		developer: true,
+		developer: false,
 		searchdescription: true,
 		searcheverywhere: true,
 		animations: true,
